9c15d41e7fa03e8766e13161715e73143c544009
liu <1203733192@qq.com>
2024-04-24 00:36:23 +0800

    get,post

---
+++ src/main/java/com/liu/day01/http/GetServlet.java
@@ -0,0 +1,21 @@
+package com.liu.day01.http;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/getServlet")
+public class GetServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("get...");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
+
