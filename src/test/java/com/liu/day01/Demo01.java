589b7661aae5d762d5dc50ef1d0df8b44baadc12
liu <1203733192@qq.com>
2024-04-23 16:00:36 +0800

    idea创建的第一个web项目

---
+++ src/test/java/com/liu/day01/Demo01.java
@@ -0,0 +1,20 @@
+package com.liu.day01;
+
+import org.junit.jupiter.api.Test;
+
+public class Demo01 {
+    @Test
+    public void test01(){
+        System.out.println("test01");
+    }
+    @Test
+    public void test02(){
+        System.out.println("test02");
+    }
+
+    @Test
+    public void test03(){
+        System.out.println("test03");
+    }
+
+}
