c80a83edee20731abee662fa78ffab3bca182567
liu <1203733192@qq.com>
2024-04-25 14:06:18 +0800

    response响应字符数据

--- src/main/java/com/liu/day01/response/ResponseDemo02.java
+++ src/main/java/com/liu/day01/response/ResponseDemo02.java
@@ -6,11 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/response02")
 public class ResponseDemo02 extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("response2");
+        //response响应字符数据
+        response.setContentType("text/html;charset=utf-8");
+        //1.获取字符输入流
+        PrintWriter writer = response.getWriter();
+        writer.write("aaa刘");
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
